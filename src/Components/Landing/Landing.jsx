0837259c227a50cfef7fcdbd378ceca27e0b7935
aniaxb <a.banachowicz11@gmail.com>
2023-04-05 13:13:37 +0200

    Removed "rounded"

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -16,7 +16,6 @@ return (
             src={
               "https://images.unsplash.com/photo-1569336415962-a4bd9f69cd83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80"
             }
-            rounded
           />
           <FontAwesomeIcon
             color="grey"
