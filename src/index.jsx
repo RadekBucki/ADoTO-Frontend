cbb2cb78fa0b92aad3321b3ce3f8b6608740e80b
Ryszawy <Krafi604@gmail.com>
2023-04-11 21:53:29 +0200

    fixed problems

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
 
