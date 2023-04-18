7f49e79a816199e60fd53db7876439161eddd525
JMielczarek02 <102308814+JMielczarek02@users.noreply.github.com>
2023-04-18 19:02:54 +0200

    add handlers

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,32 +1,39 @@
 import { MapContainer, TileLayer, Marker, Popup, FeatureGroup } from "react-leaflet";
 import { Edit, Icon } from "leaflet";
+import { useState, useRef } from "react";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 window.type = true;
 const Map = () => {
+    const drawnItemsRef = useRef(null);
+    let [coordinates, setCoordinates] = useState([]);
     const position = [51.76, 19.46];
-
     const customIcon = new Icon({
         iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.6.0/images/marker-icon.png",
         iconSize: [20, 20],
     });
 
     const handleCreate = (e) => {
-        console.log(e.layer.getLatLngs());
+        if (drawnItemsRef.current) {
+            drawnItemsRef.current.clearLayers();
+            drawnItemsRef.current.addLayer(e.layer);
+        }
+        setCoordinates(e.layer.getLatLngs()[0]);
     };
     const handleEdit = (e) => {
-        console.log(e);
+        setCoordinates(e.layer.getLatLngs()[0]);
     };
     const handleDelete = (e) => {
-        console.log(e);
+        setCoordinates([]);
     };
+
     return (
         <section className="map-component">
             <div className="map">
                 <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
-                    <FeatureGroup>
+                    <FeatureGroup ref={drawnItemsRef}>
                         <EditControl
                             position="topright"
                             onCreated={handleCreate}
