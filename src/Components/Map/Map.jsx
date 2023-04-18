d4b53772112a94a944a417ea99e9cd62a2aeb7b8
aniaxb <236493@edu.p.lodz.pl>
2023-04-18 23:55:32 +0200

    Added displaying Sidebar in Map component Coordinates tab in Sidebar displays 4 coordinates of selected rectangle

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,19 +1,16 @@
-import { MapContainer, TileLayer, Marker, Popup, FeatureGroup } from "react-leaflet";
-import { Edit, Icon } from "leaflet";
+import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
 import { useState, useRef } from "react";
 import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import Sidebar from "../Sidebar/Sidebar";
 window.type = true;
+
 const Map = () => {
     const drawnItemsRef = useRef(null);
     let [coordinates, setCoordinates] = useState([]);
     const position = [51.76, 19.46];
-    const customIcon = new Icon({
-        iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.6.0/images/marker-icon.png",
-        iconSize: [20, 20],
-    });
 
     const handleCreate = (e) => {
         if (drawnItemsRef.current) {
@@ -32,43 +29,40 @@ const Map = () => {
     };
 
     return (
-        <section className="map-component">
-            <div className="map">
-                <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
-                    <FeatureGroup ref={drawnItemsRef}>
-                        <EditControl
-                            position="topright"
-                            onCreated={handleCreate}
-                            onEdited={handleEdit}
-                            onDeleted={handleDelete}
-                            draw={{
-                                polygon: false,
-                                polyline: false,
-                                circle: false,
-                                circlemarker: false,
-                                marker: false,
-                            }}
-                        />
-                    </FeatureGroup>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={position} icon={customIcon}>
-                        <Popup>Clicked🎉</Popup>
-                    </Marker>
-                </MapContainer>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-lg-9">
+                    <section className="map-component">
+                        <div className="map">
+                            <MapContainer center={position} zoom={6} scrollWheelZoom={true}>
+                                <FeatureGroup ref={drawnItemsRef}>
+                                    <EditControl
+                                        position="topright"
+                                        onCreated={handleCreate}
+                                        onEdited={handleEdit}
+                                        onDeleted={handleDelete}
+                                        draw={{
+                                            polygon: false,
+                                            polyline: false,
+                                            circle: false,
+                                            circlemarker: false,
+                                            marker: false,
+                                        }}
+                                    />
+                                </FeatureGroup>
+                                <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                            </MapContainer>
+                        </div>
+                    </section>
+                </div>
+                <div className="col-lg-3">
+                    <Sidebar coordinates={coordinates} />
+                </div>
             </div>
-            <div>
-                {coordinates.map((cord, index) => (
-                    <div key={index}>
-                        <p>
-                            {cord.lng} x {cord.lat}
-                        </p>
-                    </div>
-                ))}
-            </div>
-        </section>
+        </div>
     );
 };
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,60 +1,56 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
 
-Sidebar = () => {
+const Sidebar = (props) => {
     const [activeTab, setActiveTab] = useState("coordinates");
     const [showCoordinates, setShowCoordinates] = useState(false);
-    const [showLayers, setShowLayers] = useState(false);
+    const { coordinates } = props;
 
     const handleTabClick = (tabName) => {
         setActiveTab(tabName);
         if (tabName === "coordinates") {
             setShowCoordinates(!showCoordinates);
-        } else if (tabName === "layers") {
-            setShowLayers(!showLayers);
         }
     };
 
-    const coordinatesGeoJSON = {
-        "type": "FeatureCollection",
-        "features": [{
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [0, 0]
-            }
-        }]
-    };
-
     return (
-        <div className="sidebar">
+        <div className="sidebar mx-2">
             <ul className="nav nav-tabs flex-column">
                 <li className="nav-item">
                     <a className={`nav-link ${activeTab === "coordinates" ? "active" : ""}`} onClick={() => handleTabClick("coordinates")}>
                         Coordinates
                     </a>
                     <div className="tab-content" id="nav-tabContent">
-                        <div className={`tab-pane fade ${showCoordinates ? 'show active' : ''}`} id="nav-coordinates" role="tabpanel" aria-labelledby="nav-coordinates-tab">
+                        <div className={`tab-pane fade ${showCoordinates ? 'show active' : ''}`} id="nav-coordinates" aria-labelledby="nav-coordinates-tab">
                             <div className="card">
                                 <div className="card-header">Selected Coordinates</div>
                                 <div className="card-body">
                                     <div>
-
-                                        <h5>Coordinates in GeoJSON format:</h5>
-                                        <pre>{JSON.stringify(coordinatesGeoJSON, null, 2)}</pre>
+                                        <p className="fs-4 fw-bold text-center">Coordinates:</p>
+                                        <div>
+                                            {coordinates.map((cord, index) => (
+                                                <div key={index}>
+                                                    <div className="row">
+                                                        <div className="col-2 d-flex align-items-center justify-content-center">
+                                                            <div className="fw-semibold ">{index + 1}</div>
+                                                        </div>
+                                                        <div className="col-10 d-flex align-items-center justify-content-center">
+                                                            <div className="text-light rounded px-1 m-1" id="cordBox">
+                                                                <p>
+                                                                    longtitude: {cord.lng}
+                                                                </p>
+                                                                <p>
+                                                                    latitude: {cord.lat}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className={`nav-link ${activeTab === "layers" ? "active" : ""}`} onClick={() => handleTabClick("layers")}>
-                        Layers
-                    </a>
-                    <div className={`card tab-pane fade ${showLayers ? 'show active' : ''}`} id="nav-layers" role="tabpanel" aria-labelledby="nav-layers-tab">
-                        <div className="card-header">Available Layers</div>
-                        <div className="card-body">
                         </div>
                     </div>
                 </li>
