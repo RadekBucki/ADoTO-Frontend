9c969ae10f7da30cafbd3027bb8e4a1ca5e39421
aniaxb <236493@edu.p.lodz.pl>
2023-06-19 20:51:32 +0200

    added instructions component on how to use the app

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,25 +1,30 @@
 import classes from './Landing.module.css';
 import Navbar from "../Navbar/Navbar";
 import ImageCarousel from "../ImageCarousel/ImageCarousel";
+import Instructions from '../Instructions/Instructions';
 
 function Landing() {
 
-return (
+  return (
     <div className={classes.landing}>
-        <img src="\src\assets\globe_img.avif"  height={1080} style={{objectFit: 'cover', width: '100%', height: '100vh'}}></img>
-        <Navbar/>
+      <img src="\src\assets\globe_img.avif" height={1080} style={{ objectFit: 'cover', width: '100%', height: '100vh' }}></img>
+      <Navbar />
 
-        <div className={classes.welcome_text}>
-          <span className={classes.welcome_line}>WELCOME</span>
-          <span className={classes.welcome_line}>TO</span>
-          <span className={classes.welcome_line}>ADoTO</span>
-        </div>
+      <div className={classes.welcome_text}>
+        <span className={classes.welcome_line}>WELCOME</span>
+        <span className={classes.welcome_line}>TO</span>
+        <span className={classes.welcome_line}>ADoTO</span>
+      </div>
 
-        <h1>What is ADoTO?</h1>
+      <div className='container'>
+        <h1 className='my-3'>What is ADoTO?</h1>
         <p>Automatic Detection of Topographic Objects (ADoTO) is a project aimed at developing a system for the automatic detection of topographic objects. The goal of this project is to create tools based on machine learning algorithms that enable the automatic detection of topographic objects such as buildings, structures, roads, rivers, fields, and forests. The detected objects will be accurately located and their data will be stored in a database. The automatic detection process will rely on photogrammetric data, such as those available in the national geodetic and cartographic resources.</p>
-        <ImageCarousel/>
+        <ImageCarousel />
+        <Instructions />
+      </div>
+
     </div>
-);
+  );
 }
 
 export default Landing
