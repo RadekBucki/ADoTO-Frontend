53f386c161b64c020014595fe51bda7d306dd5ae
Paweł Dera <paweldera01@gmail.com>
2023-06-20 14:28:51 +0200

    Imp issues component

--- src/Components/Issues/Issues.jsx
+++ src/Components/Issues/Issues.jsx
@@ -11,14 +11,56 @@ const Issues = () => {
     };
 
     return (
-            <div>
-                <div className="container-fluid vh-100 bg-dark">
-                        <p>lorem ipsum</p>
-                        <p>lorem ipsum</p>
-                        <p>lorem ipsum</p>
-                        <p>lorem ipsum</p>
-                        <p>lorem ipsum</p>
+            <div className={`${classes.backgroundIssues}  container-fluid vh-100`}>
+
+                <div className={`${classes.mediumPadding} d-flex flex-column justify-content-center align-items-center`}>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h2 className="mx-3">Experiencing issues?</h2>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h5 className="mx-3">Please check what error you are receiving before contacting us</h5>
+                        </div>
+                    </div>
                 </div>
+
+                <div className={`${classes.smallPadding} d-flex flex-row align-items-center justify-content-center`}>
+                    <img />
+                    <img />
+                </div>
+
+                <div className={`${classes.smallPadding} d-flex flex-column justify-content-center align-items-center`}>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h2 className="mx-3">What if you receive error 400 or 404?</h2>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h5 className="mx-3">Just refresh</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={`${classes.smallPadding} d-flex flex-row align-items-center justify-content-center`}>
+                    <img />
+                </div>
+
+                <div className={`${classes.smallPadding} d-flex flex-column justify-content-center align-items-center`}>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h2 className="mx-3">Other errors?</h2>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center align-items-center">
+                        <div className="col">
+                            <h5 className="mx-3">Ask geoportal</h5>
+                        </div>
+                    </div>
+                </div>
+
                 <Navbar />
             </div>
     );
