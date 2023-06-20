410857c99d8299673b0c391c4c4739aac0ca745f
Paweł Dera <paweldera01@gmail.com>
2023-06-20 16:04:54 +0200

    Add images for issues site

--- src/Components/Issues/Issues.jsx
+++ src/Components/Issues/Issues.jsx
@@ -1,19 +1,15 @@
 import Navbar from "../Navbar/Navbar";
 import classes from "../Issues/Issues.module.css";
+import imgMeme1 from "../../assets/issues_meme_1.png";
+import imgMeme2 from "../../assets/issues_meme_2.png";
+import imgGeoportal from "../../assets/issues_geoportal.png";
+import imgReload from "../../assets/issues_reload.png";
 
 const Issues = () => {
-    const config = {
-        url: import.meta.env.VITE_BASE_URL,
-        bud: import.meta.env.VITE_BUD,
-        river: import.meta.env.VITE_RIVER,
-        forest: import.meta.env.VITE_FOREST,
-        road: import.meta.env.VITE_ROAD,
-    };
-
     return (
-            <div className={`${classes.backgroundIssues}  container-fluid vh-100`}>
+            <div className={`${classes.backgroundIssues}  container-fluid`}>
 
-                <div className={`${classes.mediumPadding} d-flex flex-column justify-content-center align-items-center`}>
+                <div className={`${classes.mediumPaddingTop} d-flex flex-column justify-content-center align-items-center`}>
                     <div className="row justify-content-center align-items-center">
                         <div className="col">
                             <h2 className="mx-3">Experiencing issues?</h2>
@@ -26,12 +22,12 @@ const Issues = () => {
                     </div>
                 </div>
 
-                <div className={`${classes.smallPadding} d-flex flex-row align-items-center justify-content-center`}>
-                    <img />
-                    <img />
+                <div className={`${classes.smallPaddingTop} d-flex flex-row align-items-center justify-content-center`}>
+                    <img src={imgMeme1} alt="Meme describing how geoportal works" className="p-5"/>
+                    <img src={imgMeme2} alt="Second meme describing how geoportal works" className="p-5"/>
                 </div>
 
-                <div className={`${classes.smallPadding} d-flex flex-column justify-content-center align-items-center`}>
+                <div className={`${classes.smallPaddingTop} d-flex flex-column justify-content-center align-items-center`}>
                     <div className="row justify-content-center align-items-center">
                         <div className="col">
                             <h2 className="mx-3">What if you receive error 400 or 404?</h2>
@@ -44,11 +40,11 @@ const Issues = () => {
                     </div>
                 </div>
 
-                <div className={`${classes.smallPadding} d-flex flex-row align-items-center justify-content-center`}>
-                    <img />
+                <div className={`${classes.smallPaddingTop} d-flex flex-row align-items-center justify-content-center`}>
+                    <img src={imgReload} alt="Reload icon" className={classes.reloadImage}/>
                 </div>
 
-                <div className={`${classes.smallPadding} d-flex flex-column justify-content-center align-items-center`}>
+                <div className={`${classes.smallPaddingTop} d-flex flex-column justify-content-center align-items-center`}>
                     <div className="row justify-content-center align-items-center">
                         <div className="col">
                             <h2 className="mx-3">Other errors?</h2>
@@ -59,6 +55,10 @@ const Issues = () => {
                             <h5 className="mx-3">Ask geoportal</h5>
                         </div>
                     </div>
+                </div>
+
+                <div className={`${classes.smallPaddingTop} ${classes.mediumPaddingBottom} d-flex flex-row align-items-center justify-content-center`}>
+                    <img src={imgGeoportal} alt="Geoportal logo"/>
                 </div>
 
                 <Navbar />
